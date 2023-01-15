a31526134eac80ce4a2ab5e488daad980a5033e8
damPe <pedamovski@gmail.com>
2023-01-15 17:47:22 +0100

    added functionality up to sum funciton

--- script/app.js
+++ script/app.js
@@ -1,15 +1,36 @@
 const display = document.getElementById("display");
 const clearDisplay = document.querySelector(".clear");
-const Delete = document.querySelector("del");
+const Delete = document.querySelector(".del");
+const equal = document.querySelector(".equal");
+const display2 = document.querySelector(".displayBackup");
 
+// funciton Buttons
+const substract = document.querySelector(".substract");
+const multyply = document.querySelector(".multyply");
+const devide = document.querySelector(".devide");
+const equal2 = document.querySelector(".equal");
+// function to delete the last number in teh display panel
+
+const deleteNum = () => {
+  const temp = display.textContent.length;
+  if (temp === 1) {
+    display.textContent = "0";
+  } else {
+    display.textContent = display.textContent.substring(0, temp - 1);
+  }
+  return;
+};
+Delete.addEventListener("click", deleteNum);
+
+// Put to all buttons, event listeners with the gutNum function
 const numberbutton = document.querySelectorAll(".calButton");
 for (const iterator of numberbutton) {
   iterator.addEventListener("click", function () {
     const number = iterator.innerText;
-    console.log(number);
     getNum(number);
   });
 }
+// function to write the number to the display panel,or
 
 function getNum(numberToAdd) {
   if (display.textContent.length < 15) {
@@ -22,15 +43,16 @@ function getNum(numberToAdd) {
     display.textContent === 0;
   }
 }
+// function to clear the display panel by writing 0 to the text content
 
-clearDisplay.addEventListener("click", function () {
+function turnScreenTo0() {
   display.textContent = "0";
   return;
-});
-
-function sum(a, b) {
-  return a + b;
 }
+// add event listener with the turn screen to 0 function
+
+clearDisplay.addEventListener("click", turnScreenTo0);
+
 function divisinon(a, b) {
   return a / b;
 }
@@ -43,13 +65,39 @@ function multiply(a, b) {
   return a * b;
 }
 
-const add = document.querySelector("add");
-const substract = document.querySelectorAll("substract");
-const multyply = documentation.querySelector("multyply");
-const devide = document.querySelector("devide");
-const equal = document.querySelector("equal");
+const add = document.querySelector(".add");
+add.removeEventListener("click", addFun);
+add.addEventListener("click", addFun);
 
-add.addEventListener("click", function () {
+function sum(a, b) {
+  a = parseInt(a);
+  b = parseInt(b);
+  return a + b;
+}
+function addFun() {
+  // add.removeEventListener("click", addFun);
+  let turn = 0;
+
+  const re = /\d*/;
+
+  const filterednum = display.textContent.match(re).toString();
+
+  display2.textContent = filterednum;
+
+  const memoryContent = display2.textContent;
+
+  turnScreenTo0();
+  console.log(`this is memory contnt :${memoryContent}`);
+
   const screenNumber = display.textContent;
-  clearDisplay();
-});
+
+  let result;
+
+  if (turn !== 0 && memoryContent.length > 0) {
+    result = parseInt(sum(memoryContent, screenNumber));
+    console.log(`this is the result:${result}`);
+  }
+  turn++;
+
+  display.textContent = result;
+}
