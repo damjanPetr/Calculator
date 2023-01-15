d737bb6cc839084b03b0296a7565071f78b377c7
damPe <pedamovski@gmail.com>
2023-01-15 23:04:09 +0100

    add operator functionality

--- script/app.js
+++ script/app.js
@@ -1,14 +1,18 @@
 const display = document.getElementById("display");
 const clearDisplay = document.querySelector(".clear");
 const Delete = document.querySelector(".del");
-const equal = document.querySelector(".equal");
+// const equal = document.querySelector(".equal");
 const display2 = document.querySelector(".displayBackup");
+const diplays = document.getElementById("symboldisplay");
 
-// funciton Buttons
+const add = document.querySelector(".add");
+
+// function Buttons
 const substract = document.querySelector(".substract");
 const multyply = document.querySelector(".multyply");
 const devide = document.querySelector(".devide");
 const equal2 = document.querySelector(".equal");
+
 // function to delete the last number in teh display panel
 
 const deleteNum = () => {
@@ -20,9 +24,11 @@ const deleteNum = () => {
   }
   return;
 };
+
 Delete.addEventListener("click", deleteNum);
 
 // Put to all buttons, event listeners with the gutNum function
+
 const numberbutton = document.querySelectorAll(".calButton");
 for (const iterator of numberbutton) {
   iterator.addEventListener("click", function () {
@@ -30,9 +36,13 @@ for (const iterator of numberbutton) {
     getNum(number);
   });
 }
+
 // function to write the number to the display panel,or
+let content = 0;
 
 function getNum(numberToAdd) {
+  // console.log(`${content}`);
+
   if (display.textContent.length < 15) {
     if (display.textContent === "0") {
       display.textContent = "";
@@ -45,59 +55,108 @@ function getNum(numberToAdd) {
 }
 // function to clear the display panel by writing 0 to the text content
 
-function turnScreenTo0() {
+function turnBOTHScreenTo0() {
+  display.textContent = "0";
+  display2.textContent = "0";
+  content = 0;
+
+  return;
+}
+function turnSINGLEScreenTo0() {
   display.textContent = "0";
   return;
 }
 // add event listener with the turn screen to 0 function
 
-clearDisplay.addEventListener("click", turnScreenTo0);
+clearDisplay.addEventListener("click", turnBOTHScreenTo0);
 
 function divisinon(a, b) {
-  return a / b;
+  if (b === "") {
+    b = 0;
+  }
+  a = Number.parseInt(a);
+  b = Number.parseInt(b);
+  let c = Number.parseInt(a / b);
+  turnSINGLEScreenTo0();
+  // console.log(typeof c);
+  return c;
 }
 
 function subtraction(a, b) {
-  return a / b;
+  if (b == "") {
+    b = 0;
+  }
+  a = Number.parseInt(a);
+  b = Number.parseInt(b);
+  let c = Number.parseInt(a - b);
+  turnSINGLEScreenTo0();
+  // console.log(typeof c);
+  return c;
 }
 
 function multiply(a, b) {
-  return a * b;
-}
-
-const add = document.querySelector(".add");
-add.removeEventListener("click", addFun);
-add.addEventListener("click", addFun);
-
-function sum(a, b) {
-  a = parseInt(a);
-  b = parseInt(b);
-  return a + b;
-}
-function addFun() {
-  // add.removeEventListener("click", addFun);
-  let turn = 0;
-
-  const re = /\d*/;
-
-  const filterednum = display.textContent.match(re).toString();
-
-  display2.textContent = filterednum;
-
-  const memoryContent = display2.textContent;
-
-  turnScreenTo0();
-  console.log(`this is memory contnt :${memoryContent}`);
-
-  const screenNumber = display.textContent;
-
-  let result;
-
-  if (turn !== 0 && memoryContent.length > 0) {
-    result = parseInt(sum(memoryContent, screenNumber));
-    console.log(`this is the result:${result}`);
+  if (b == "") {
+    b = 0;
   }
-  turn++;
+  a = Number.parseInt(a);
+  b = Number.parseInt(b);
+  let c = Number.parseInt(a * b);
+  turnSINGLEScreenTo0();
+  // console.log(typeof c);
+  return c;
+}
 
-  display.textContent = result;
+function addition(a, b) {
+  if (b == "") {
+    b = 0;
+  }
+  a = Number.parseInt(a);
+  b = Number.parseInt(b);
+  let c = Number.parseInt(a + b);
+  turnSINGLEScreenTo0();
+  // console.log(typeof c);
+  return c;
+}
+
+const operators = document.querySelectorAll(".operator");
+
+for (const iterator of operators) {
+  iterator.addEventListener("click", (e) => {
+    operate(e.target.textContent);
+  });
+}
+
+function operate(arg) {
+  if (arg === "+") {
+    console.log(`${display2.textContent} THIS is greed display `);
+
+    content += Number.parseInt(display.textContent);
+
+    display2.textContent = addition(display.textContent, display2.textContent);
+    // display2.textContent = addition(display.textContent, content);
+  } else if (arg === "-") {
+    content = content + Number.parseInt(display.textContent);
+
+    display.textContent = subtraction(
+      display.textContent,
+      display2.textContent
+    );
+  } else if (arg === "*") {
+    content *= Number.parseInt(display.textContent);
+
+    display.textContent = multiply(display.textContent, display2.textContent);
+  } else if (arg === "/") {
+    display.textContent = divisinon(display.textContent, display2.textContent);
+
+    content = content + Number.parseInt(display.textContent);
+  } else if (arg === "=") {
+    content = content + Number.parseInt(display.textContent);
+    display.textContent = content;
+    display2.textContent = 0;
+    content = 0;
+    console.log("uhtneoahutneoahutnao");
+  }
+  console.log(`first: ${content}`);
+
+  return;
 }
